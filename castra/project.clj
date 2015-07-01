03e6356e9ed9a2111d788b4c368dfadb87516990
Michael Berg <michael.berg@zalf.de>
2015-07-01 13:38:31 +0200

    version bump

--- castra/project.clj
+++ castra/project.clj
@@ -1,5 +1,5 @@
 (defproject
-  de.zalf.berest/berest-castra-service "0.0.3"
+  de.zalf.berest/berest-castra-service "0.0.4"
 
   :description "BEREST CASTRA service"
   :url "http://example.com/FIXME"
@@ -7,7 +7,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
 
-                 [de.zalf.berest/berest-core "0.0.3"]
+                 [de.zalf.berest/berest-core "0.0.4"]
 
                  [tailrecursion/castra "2.2.2"]
 
