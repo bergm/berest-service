64027da7a78e93d70ba8afe1db7148e48ce04fd9
Michael Berg <michael.berg@zalf.de>
2016-01-14 13:04:24 +0100

    lib update and version bump

--- castra/project.clj
+++ castra/project.clj
@@ -1,33 +1,35 @@
 (defproject
-  de.zalf.berest/berest-castra-service "0.0.7"
+  de.zalf.berest/berest-castra-service "0.0.9"
 
   :description "BEREST CASTRA service"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
 
-                 [de.zalf.berest/berest-core "0.0.7"]
+                 [de.zalf.berest/berest-core "0.0.9"]
 
                  [tailrecursion/castra "2.2.2"]
 
-                 [com.datomic/datomic-pro "0.9.5173" :exclusions [joda-time]]
+                 [com.datomic/datomic-pro "0.9.5344" :exclusions [joda-time]]
+                 [com.amazonaws/aws-java-sdk-dynamodb "1.9.39" :exclusions [joda-time]]
 
-                 [clojurewerkz/quartzite "1.3.0"]
+                 [clojurewerkz/quartzite "2.0.0"]
 
-                 [org.zeromq/jeromq "0.3.4"]
+                 [org.zeromq/jeromq "0.3.5"]
                  [org.zeromq/cljzmq "0.1.4" :exclusions [org.zeromq/jzmq]]
 
-                 [ring "1.2.1"]
-                 [ring-server "0.3.1"]
-                 [ring-cors "0.1.7"]
-                 [fogus/ring-edn "0.2.0"]
+                 [ring "1.4.0"]
+                 [ring-server "0.4.0"]
+                 [jumblerg/ring.middleware.cors "1.0.1"]
+                 #_[ring-cors "0.1.7"]
+                 [fogus/ring-edn "0.3.0"]
 
-                 [simple-time "0.1.1"]
-                 [clj-time "0.9.0"]
+                 [simple-time "0.2.1"]
+                 [clj-time "0.11.0"]
 
                  [clojure-csv "2.0.1"]
-                 [org.clojure/core.match "0.2.0"]]
+                 [org.clojure/core.match "0.2.2"]]
   :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
                                    :username "michael.berg@zalf.de"
                                    :password "dfe713b3-62f0-469d-8ac9-07d6b02b0175"}}
@@ -42,8 +44,8 @@
                    :resource-paths ["../../berest-hoplon-website"]}}
 
   :ring {:handler de.zalf.berest.web.castra.handler/castra-service
-         :init de.zalf.berest.core.import.dwd-data/start-import-scheduler
-         :destroy de.zalf.berest.core.import.dwd-data/stop-import-scheduler
+         ;:init de.zalf.berest.core.import.dwd-data/start-import-scheduler
+         ;:destroy de.zalf.berest.core.import.dwd-data/stop-import-scheduler
          }
 
   :main de.zalf.berest.web.castra.run-dev-server
